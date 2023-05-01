f7336ab78eb3b22b6708058a6b1d92ae28a8d853
eleanore-ritter <44507907+eleanore-ritter@users.noreply.github.com>
2023-05-01 16:25:15 -0400

    Update GO_term_enrichment.R

--- scripts/GO_term_enrichment.R
+++ scripts/GO_term_enrichment.R
@@ -1,17 +1,31 @@
 # Set working directory and load necessary packages
-setwd("C:/Users/rittere5/OneDrive - Michigan State University/Vitis-domatia/")
-#setwd("C:/Users/elean/OneDrive - Michigan State University/Vitis-domatia/")
+#setwd("C:/Users/rittere5/OneDrive - Michigan State University/Vitis-domatia/")
+setwd("C:/Users/elean/OneDrive - Michigan State University/Vitis-domatia/")
 
 library("topGO")
 
+######################## GO TERM ENRICHMENT WITH DOMATIA V LEAF IN 588710 ######################## 
+
 # Load data
-all <- read.csv("Vriparia-functional-annotations-V2.tsv", sep='\t')
+
+## Load functional annotations
+all <- read.csv("Vriparia-functional-annotations.tsv", sep='\t')
+
+## Add gene data to all if needed
+dict <- read.csv("cds-to-gene.tsv", sep = '\t')
+colnames(dict) <- c("Transcript", "gene")
+all <- merge(all, dict, by="Transcript", all = TRUE)
+all <- all[,c(1, 8, 2:7)]
+all <- all %>% drop_na(gene)
+all <- all[!duplicated(all),]
+
+## Load differentially expressed genes
 degenes <- read.csv("DE_genes_Domatia_V_Leaf_588710.csv")
 
 # Modify data
 
 ## Modify GO term file
-allgo <- all[,c(2,5)]
+allgo <- all[,c(2,7)]
 rn1 <- paste(allgo[,1], sep="")
 gene2GO = allgo[,-1]
 names(gene2GO)<-rn1
@@ -27,13 +41,12 @@ ft <- function(x){
   return(x>0)
 }
 
-## Set up as topGOdata
+## Set up as topGOdata with BP ontology
 GODATA <- new("topGOdata",
               description = "Domatia vs Leaf in 588710",
-              ontology = "CC",
+              ontology = "BP",
               allGenes = matches,
               gene2GO = gene2GO,
-              nodeSize = 10,
               geneSel = ft,
               annotationFun = annFUN.gene2GO
               )
@@ -41,12 +54,390 @@ GODATA <- new("topGOdata",
 # Run GO term enrichment analysis
 
 ## Run with Fisher's exact test
-resultFisher <- runTest(GODATA, algorithm = "classic", statistic = "fisher")
-resultFisher
+# resultFisher.BP <- runTest(GODATA, algorithm = "classic", statistic = "fisher")
+# resultFisher.BP
 
 ## Run with Kolmogorov-Smirnov test
-resultKS <- runTest(GODATA, algorithm = "classic", statistic = "ks") # Classic method
-resultKS
+resultKS.BP710 <- runTest(GODATA, algorithm = "classic", statistic = "ks") # Classic method
+resultKS.BP710
+pvalKS.BP710 <- as.data.frame(score(resultKS.BP710))
 
-resultKS.elim <- runTest(GODATA, algorithm = "elim", statistic = "ks") # Elimination method
-resultKS.elim
+# resultKS.elim.BP <- runTest(GODATA, algorithm = "elim", statistic = "ks") # Elimination method
+# resultKS.elim.BP
+
+## Set up as topGOdata with MF ontology
+GODATA <- new("topGOdata",
+              description = "Domatia vs Leaf in 588710",
+              ontology = "MF",
+              allGenes = matches,
+              gene2GO = gene2GO,
+              geneSel = ft,
+              annotationFun = annFUN.gene2GO
+)
+
+# Run GO term enrichment analysis
+
+# ## Run with Fisher's exact test
+# resultFisher.MF <- runTest(GODATA, algorithm = "classic", statistic = "fisher")
+# resultFisher.MF
+
+## Run with Kolmogorov-Smirnov test
+resultKS.MF710 <- runTest(GODATA, algorithm = "classic", statistic = "ks") # Classic method
+resultKS.MF710
+pvalKS.MF710 <- as.data.frame(score(resultKS.MF710))
+
+# resultKS.elim.MF <- runTest(GODATA, algorithm = "elim", statistic = "ks") # Elimination method
+# resultKS.elim.MF
+
+## Set up as topGOdata with CC ontology
+GODATA <- new("topGOdata",
+              description = "Domatia vs Leaf in 588710",
+              ontology = "CC",
+              allGenes = matches,
+              gene2GO = gene2GO, 
+              geneSel = ft,
+              annotationFun = annFUN.gene2GO
+)
+
+# Run GO term enrichment analysis
+
+## Run with Fisher's exact test
+# resultFisher.CC <- runTest(GODATA, algorithm = "classic", statistic = "fisher")
+# resultFisher.CC
+
+## Run with Kolmogorov-Smirnov test
+resultKS.CC710 <- runTest(GODATA, algorithm = "classic", statistic = "ks") # Classic method
+resultKS.CC710
+pvalKS.CC710 <- as.data.frame(score(resultKS.CC710))
+
+# resultKS.elim.CC <- runTest(GODATA, algorithm = "elim", statistic = "ks") # Elimination method
+
+######################## GO TERM ENRICHMENT WITH DOMATIA V LEAF IN 588711 ######################## 
+
+# Load data
+
+## Load functional annotations
+all <- read.csv("Vriparia-functional-annotations.tsv", sep='\t')
+
+## Add gene data to all if needed
+dict <- read.csv("cds-to-gene.tsv", sep = '\t')
+colnames(dict) <- c("Transcript", "gene")
+all <- merge(all, dict, by="Transcript", all = TRUE)
+all <- all[,c(1, 8, 2:7)]
+all <- all %>% drop_na(gene)
+all <- all[!duplicated(all),]
+
+## Load differentially expressed genes
+degenes <- read.csv("DE_genes_Domatia_V_Leaf_588711.csv")
+
+# Modify data
+
+## Modify GO term file
+allgo <- all[,c(2,7)]
+rn1 <- paste(allgo[,1], sep="")
+gene2GO = allgo[,-1]
+names(gene2GO)<-rn1
+gene2GO<-strsplit(gene2GO,"\\|")
+
+## Modify DE gene list
+matches<-setNames(match(degenes$Gene,allgo$gene,nomatch=0),degenes$Gene)
+matches[matches>0]<-1
+
+# Set up GO data
+## Set up function
+ft <- function(x){
+  return(x>0)
+}
+
+## Set up as topGOdata with BP ontology
+GODATA <- new("topGOdata",
+              description = "Domatia vs Leaf in 588711",
+              ontology = "BP",
+              allGenes = matches,
+              gene2GO = gene2GO,
+              geneSel = ft,
+              annotationFun = annFUN.gene2GO
+)
+
+# Run GO term enrichment analysis
+
+## Run with Fisher's exact test
+# resultFisher.BP <- runTest(GODATA, algorithm = "classic", statistic = "fisher")
+# resultFisher.BP
+
+## Run with Kolmogorov-Smirnov test
+resultKS.BP711 <- runTest(GODATA, algorithm = "classic", statistic = "ks") # Classic method
+resultKS.BP711
+pvalKS.BP711 <- as.data.frame(score(resultKS.BP711))
+
+# resultKS.elim.BP <- runTest(GODATA, algorithm = "elim", statistic = "ks") # Elimination method
+# resultKS.elim.BP
+
+## Set up as topGOdata with MF ontology
+GODATA <- new("topGOdata",
+              description = "Domatia vs Leaf in 588710",
+              ontology = "MF",
+              allGenes = matches,
+              gene2GO = gene2GO,
+              geneSel = ft,
+              annotationFun = annFUN.gene2GO
+)
+
+# Run GO term enrichment analysis
+
+# ## Run with Fisher's exact test
+# resultFisher.MF <- runTest(GODATA, algorithm = "classic", statistic = "fisher")
+# resultFisher.MF
+
+## Run with Kolmogorov-Smirnov test
+resultKS.MF711 <- runTest(GODATA, algorithm = "classic", statistic = "ks") # Classic method
+resultKS.MF711
+pvalKS.MF711 <- as.data.frame(score(resultKS.MF711))
+
+# resultKS.elim.MF <- runTest(GODATA, algorithm = "elim", statistic = "ks") # Elimination method
+# resultKS.elim.MF
+
+## Set up as topGOdata with CC ontology
+GODATA <- new("topGOdata",
+              description = "Domatia vs Leaf in 588710",
+              ontology = "CC",
+              allGenes = matches,
+              gene2GO = gene2GO, 
+              geneSel = ft,
+              annotationFun = annFUN.gene2GO
+)
+
+# Run GO term enrichment analysis
+
+## Run with Fisher's exact test
+# resultFisher.CC <- runTest(GODATA, algorithm = "classic", statistic = "fisher")
+# resultFisher.CC
+
+## Run with Kolmogorov-Smirnov test
+resultKS.CC711 <- runTest(GODATA, algorithm = "classic", statistic = "ks") # Classic method
+resultKS.CC711
+pvalKS.CC711 <- as.data.frame(score(resultKS.CC711))
+
+# resultKS.elim.CC <- runTest(GODATA, algorithm = "elim", statistic = "ks") # Elimination method
+# resultKS.elim.CC
+# resultKS.elim.CC
+
+######################## GO TERM ENRICHMENT WITH DOMATIA V DOMATIA ######################## 
+
+# Load data
+
+## Load functional annotations
+all <- read.csv("Vriparia-functional-annotations.tsv", sep='\t')
+
+## Add gene data to all if needed
+dict <- read.csv("cds-to-gene.tsv", sep = '\t')
+colnames(dict) <- c("Transcript", "gene")
+all <- merge(all, dict, by="Transcript", all = TRUE)
+all <- all[,c(1, 8, 2:7)]
+all <- all %>% drop_na(gene)
+all <- all[!duplicated(all),]
+
+## Load differentially expressed genes
+degenes <- read.csv("DE_genes_Domatia_710_v_711.csv")
+
+# Modify data
+
+## Modify GO term file
+allgo <- all[,c(2,7)]
+rn1 <- paste(allgo[,1], sep="")
+gene2GO = allgo[,-1]
+names(gene2GO)<-rn1
+gene2GO<-strsplit(gene2GO,"\\|")
+
+## Modify DE gene list
+matches<-setNames(match(degenes$Gene,allgo$gene,nomatch=0),degenes$Gene)
+matches[matches>0]<-1
+
+# Set up GO data
+## Set up function
+ft <- function(x){
+  return(x>0)
+}
+
+## Set up as topGOdata with BP ontology
+GODATA <- new("topGOdata",
+              description = "Domatia vs Domatia",
+              ontology = "BP",
+              allGenes = matches,
+              gene2GO = gene2GO,
+              geneSel = ft,
+              annotationFun = annFUN.gene2GO
+)
+
+# Run GO term enrichment analysis
+
+## Run with Fisher's exact test
+# resultFisher.BP <- runTest(GODATA, algorithm = "classic", statistic = "fisher")
+# resultFisher.BP
+
+## Run with Kolmogorov-Smirnov test
+resultKS.BPDOM <- runTest(GODATA, algorithm = "classic", statistic = "ks") # Classic method
+resultKS.BPDOM
+pvalKS.BPDOM <- as.data.frame(score(resultKS.BPDOM))
+
+# resultKS.elim.BP <- runTest(GODATA, algorithm = "elim", statistic = "ks") # Elimination method
+# resultKS.elim.BP
+
+## Set up as topGOdata with MF ontology
+GODATA <- new("topGOdata",
+              description = "Domatia vs Domatia",
+              ontology = "MF",
+              allGenes = matches,
+              gene2GO = gene2GO,
+              geneSel = ft,
+              annotationFun = annFUN.gene2GO
+)
+
+# Run GO term enrichment analysis
+
+# ## Run with Fisher's exact test
+# resultFisher.MF <- runTest(GODATA, algorithm = "classic", statistic = "fisher")
+# resultFisher.MF
+
+## Run with Kolmogorov-Smirnov test
+resultKS.MFDOM <- runTest(GODATA, algorithm = "classic", statistic = "ks") # Classic method
+resultKS.MFDOM
+pvalKS.MFDOM <- as.data.frame(score(resultKS.MFDOM))
+
+# resultKS.elim.MF <- runTest(GODATA, algorithm = "elim", statistic = "ks") # Elimination method
+# resultKS.elim.MF
+
+## Set up as topGOdata with CC ontology
+GODATA <- new("topGOdata",
+              description = "Domatia vs Domatia",
+              ontology = "CC",
+              allGenes = matches,
+              gene2GO = gene2GO, 
+              geneSel = ft,
+              annotationFun = annFUN.gene2GO
+)
+
+# Run GO term enrichment analysis
+
+## Run with Fisher's exact test
+# resultFisher.CC <- runTest(GODATA, algorithm = "classic", statistic = "fisher")
+# resultFisher.CC
+
+## Run with Kolmogorov-Smirnov test
+resultKS.CCDOM <- runTest(GODATA, algorithm = "classic", statistic = "ks") # Classic method
+resultKS.CCDOM
+pvalKS.CCDOM <- as.data.frame(score(resultKS.CCDOM))
+
+# resultKS.elim.CC <- runTest(GODATA, algorithm = "elim", statistic = "ks") # Elimination method
+# resultKS.elim.CC
+# resultKS.elim.CC
+
+######################## GO TERM ENRICHMENT WITH CONTROL V CONTROL ######################## 
+
+# Load data
+
+## Load functional annotations
+all <- read.csv("Vriparia-functional-annotations.tsv", sep='\t')
+
+## Add gene data to all if needed
+dict <- read.csv("cds-to-gene.tsv", sep = '\t')
+colnames(dict) <- c("Transcript", "gene")
+all <- merge(all, dict, by="Transcript", all = TRUE)
+all <- all[,c(1, 8, 2:7)]
+all <- all %>% drop_na(gene)
+all <- all[!duplicated(all),]
+
+## Load differentially expressed genes
+degenes <- read.csv("DE_genes_Control_710_v_711.csv")
+
+# Modify data
+
+## Modify GO term file
+allgo <- all[,c(2,7)]
+rn1 <- paste(allgo[,1], sep="")
+gene2GO = allgo[,-1]
+names(gene2GO)<-rn1
+gene2GO<-strsplit(gene2GO,"\\|")
+
+## Modify DE gene list
+matches<-setNames(match(degenes$Gene,allgo$gene,nomatch=0),degenes$Gene)
+matches[matches>0]<-1
+
+# Set up GO data
+## Set up function
+ft <- function(x){
+  return(x>0)
+}
+
+## Set up as topGOdata with BP ontology
+GODATA <- new("topGOdata",
+              description = "Control v Control",
+              ontology = "BP",
+              allGenes = matches,
+              gene2GO = gene2GO,
+              geneSel = ft,
+              annotationFun = annFUN.gene2GO
+)
+
+# Run GO term enrichment analysis
+
+## Run with Fisher's exact test
+# resultFisher.BP <- runTest(GODATA, algorithm = "classic", statistic = "fisher")
+# resultFisher.BP
+
+## Run with Kolmogorov-Smirnov test
+resultKS.BPCONTROL <- runTest(GODATA, algorithm = "classic", statistic = "ks") # Classic method
+resultKS.BPCONTROL
+pvalKS.BPCONTROL <- as.data.frame(score(resultKS.BPCONTROL))
+
+# resultKS.elim.BP <- runTest(GODATA, algorithm = "elim", statistic = "ks") # Elimination method
+# resultKS.elim.BP
+
+## Set up as topGOdata with MF ontology
+GODATA <- new("topGOdata",
+              description = "Control v Control",
+              ontology = "MF",
+              allGenes = matches,
+              gene2GO = gene2GO,
+              geneSel = ft,
+              annotationFun = annFUN.gene2GO
+)
+
+# Run GO term enrichment analysis
+
+# ## Run with Fisher's exact test
+# resultFisher.MF <- runTest(GODATA, algorithm = "classic", statistic = "fisher")
+# resultFisher.MF
+
+## Run with Kolmogorov-Smirnov test
+resultKS.MFCONTROL <- runTest(GODATA, algorithm = "classic", statistic = "ks") # Classic method
+resultKS.MFCONTROL
+pvalKS.MFCONTROL <- as.data.frame(score(resultKS.MFCONTROL))
+
+# resultKS.elim.MF <- runTest(GODATA, algorithm = "elim", statistic = "ks") # Elimination method
+# resultKS.elim.MF
+
+## Set up as topGOdata with CC ontology
+GODATA <- new("topGOdata",
+              description = "Control v Control",
+              ontology = "CC",
+              allGenes = matches,
+              gene2GO = gene2GO, 
+              geneSel = ft,
+              annotationFun = annFUN.gene2GO
+)
+
+# Run GO term enrichment analysis
+
+## Run with Fisher's exact test
+# resultFisher.CC <- runTest(GODATA, algorithm = "classic", statistic = "fisher")
+# resultFisher.CC
+
+## Run with Kolmogorov-Smirnov test
+resultKS.CCCONTROL <- runTest(GODATA, algorithm = "classic", statistic = "ks") # Classic method
+resultKS.CCCONTROL
+pvalKS.CCCONTROL <- as.data.frame(score(resultKS.CCCONTROL))
+
+# resultKS.elim.CC <- runTest(GODATA, algorithm = "elim", statistic = "ks") # Elimination method
+# resultKS.elim.CC
+# resultKS.elim.CC
