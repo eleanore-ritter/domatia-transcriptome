d9b45638ce100669078ec8c2512d678d3f75477f
eleanore-ritter <44507907+eleanore-ritter@users.noreply.github.com>
2023-05-11 17:05:00 -0400

    Update diamond.sh

--- scripts/gene_functions/diamond.sh
+++ scripts/gene_functions/diamond.sh
@@ -7,7 +7,7 @@
 #SBATCH --cpus-per-task=1           # number of CPUs (or cores) per task (same as -c)
 #SBATCH --mem=25G                   # memory required per allocated CPU (or core) - amount of memory (in bytes)
 #SBATCH --job-name diamond          # you can give your job a name for easier identification (same as -J)
-#SBATCH --output=%x_%j.SLURMout
+#SBATCH --output=job_reports/%x_%j.SLURMout
 
 #Set this variable to the path to wherever you have conda installed
 conda="${HOME}/anaconda3"
@@ -19,10 +19,20 @@ cd ${PBS_O_WORKDIR}
 export PATH="${conda}/envs/diamond/bin:${PATH}"
 export LD_LIBRARY_PATH="${conda}/envs/diamond/lib:${LD_LIBRARY_PATH}"
 
+# Arabidopsis
+# diamond blastx -d Araport11 \
+# -q ../ref/cds_from_genomic.mod.fna \
+# -o Vriparia-Athaliana.tsv \
+# --iterate \
+# --max-target-seqs 1 \
+# --unal 0
 
-diamond blastx -d Araport11 \
--q ../ref/cds_from_genomic.mod.fna \
--o Vriparia-Athaliana.tsv \
+# Vvinifera
+cd Vvinifera
+
+diamond blastx -d Vvinifera_VCost3 \
+-q ../cds_from_genomic.mod.fna \
+-o Vriparia-Vvinifera.tsv \
 --iterate \
 --max-target-seqs 1 \
 --unal 0
