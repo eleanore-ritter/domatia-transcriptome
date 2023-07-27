e4cec4489d21535a903f4b423cf9d6493610f348
eleanore-ritter <44507907+eleanore-ritter@users.noreply.github.com>
2023-07-27 15:44:20 -0400

    Update data_wrangling_for_GREAT.R

--- scripts/data_wrangling_for_GREAT.R
+++ scripts/data_wrangling_for_GREAT.R
@@ -1,14 +1,14 @@
 # Set working directory and load necessary packages
-#setwd("C:/Users/rittere5/OneDrive - Michigan State University/Vitis-domatia/")
-setwd("C:/Users/elean/OneDrive - Michigan State University/Vitis-domatia/")
+setwd("C:/Users/rittere5/OneDrive - Michigan State University/Vitis-domatia/")
+#setwd("C:/Users/elean/OneDrive - Michigan State University/Vitis-domatia/")
 
 ######################## DATA WRANGLING FOR GREAT ########################
 
 # Load data
 dict <- read.csv("Vriparia-Vvinifera.tsv", sep='\t', header = FALSE)
-df1 <- read.csv("DE_genes_Domatia_V_Leaf_588710.csv")
+df1 <- read.csv("differentially_expressed_genes/DE_genes_Domatia_V_Leaf_588710.csv")
 row.names(df1) <- df1$Gene
-df2 <- read.csv("DE_genes_Domatia_V_Leaf_588711.csv")
+df2 <- read.csv("differentially_expressed_genes/DE_genes_Domatia_V_Leaf_588711.csv")
 row.names(df2) <- df2$Gene
 
 
@@ -17,66 +17,62 @@ dom <- df1[rownames(df1)%in%rownames(df2),]
 
 # Get Vvinifera orthologs to differentially expressed genes
 vvi <- as.data.frame(merge(dict, dom, by.x = "V1", by.y = "Gene"))
-#temp1 <- as.data.frame(merge(dict, dom, by.x = "V1", by.y = "Gene")) #Good for searching gene IDs
 vvi <- vvi$V2
 vvi <- as.data.frame(gsub(".t01", "", vvi))
 colnames(vvi) <- c("V1")
 vvi<-t(vvi)
 
 # Copy data for GREAT
-#writeClipboard(paste0(as.vector(vvi),collapse=","))
+writeClipboard(paste0(as.vector(vvi),collapse=","))
 
 ######################## DE ANALYSIS WITH GREAT DATA ########################
-library(DESeq2)
-library(dplyr)
-
 # Load GREAT data into R and reformat
-data1 <- read.csv(file = "GREAT_analysis/GREAT-data/GREAT_RPKM_samples2023-07-26_inflorescence.csv", row.names = "X")
-data2 <- read.csv(file = "GREAT_analysis/GREAT-data/GREAT_RPKM_samples2023-07-26_leaves.csv", row.names = "X")
-data3 <- read.csv(file = "GREAT_analysis/GREAT-data/GREAT_RPKM_samples2023-07-26_stem.csv", row.names = "X")
-temp <- cbind(data1, data2, data3)
-counts <- mutate_all(temp, function(x) as.numeric(as.integer(x)))
-temp$Gene <- row.names(temp)
+data1 <- read.csv(file = "GREAT_analysis/GREAT-data/GREAT_RPKM_samples2023-07-27_inflorescence_flower.csv", row.names = "X")
+data2 <- read.csv(file = "GREAT_analysis/GREAT-data/GREAT_RPKM_samples2023-07-27_leaf.csv", row.names = "X")
+data3 <- read.csv(file = "GREAT_analysis/GREAT-data/GREAT_RPKM_samples2023-07-27_stem.csv", row.names = "X")
 
-colData <- read.csv(file="GREAT_analysis/great_matrix.csv")
-colData$tissue <- as.factor(colData$tissue)
+colnames(data1) <- paste("Inflorescence" , colnames(data1), sep=":")
+colnames(data2) <- paste("Leaf" , colnames(data2), sep=":")
+colnames(data3) <- paste("Stem" , colnames(data3), sep=":")
 
-##Making Col DESeq Data Set
-dds = DESeqDataSetFromMatrix(countData = counts, colData = colData,  design = ~tissue)
-#Make sure Control is the first level of treatment
-dds$tissue <- relevel(dds$tissue, ref = "Control")
-#Make sure that dds is properly formatted
-as.data.frame(colData(dds))
+counts <- cbind(data1, data2, data3)
+#counts$Gene <- row.names(counts)
 
-##Run DESeq
-dds <- DESeq(dds)
-res <- results(dds)
+genes.mat <- as.matrix(counts)
+heatmap(genes.mat, Rowv = NA, Colv = NA,margins=c(10,10))
 
-## Double check result names
-resultsNames(dds)
+# List of genes that seem to be upregulated in inflorescences compared to leaf and stem tissue
+flower <- c("Vitvi02g00512",
+            "Vitvi04g00641",
+            "Vitvi03g01477",
+            "Vitvi04g02123",
+            "Vitvi07g01906",
+            "Vitvi07g00470",
+            "Vitvi09g00463",
+            "Vitvi18g03417",
+            "Vitvi19g00369",
+            "Vitvi19g00264",
+            "Vitvi11g00247",
+            "Vitvi11g00231",
+            "Vitvi10g00854",
+            "Vitvi10g00663",
+            "Vitvi10g00644",
+            "Vitvi13g00829",
+            "Vitvi12g02361",
+            "Vitvi12g00725",
+            "Vitvi15g00776",
+            "Vitvi14g01928",
+            "Vitvi13g02416",
+            "Vitvi18g02654")
 
-## Filter differentially expressed genes between inflorescences and leaves
-deseq1 <- results(dds, contrast=c("tissue", "inflorescence", "Control"))
-df.deseq1 <- as.data.frame(deseq1)
-res1.deseq1<-df.deseq1[(df.deseq1$log2FoldChange>1|df.deseq1$log2FoldChange< -1)& !is.na(df.deseq1$log2FoldChange),]
-inflor.leaves <- res1.deseq1[(res1.deseq1$padj<0.05)&!is.na(res1.deseq1$padj),]
-inflor.leaves$Gene <- row.names(inflor.leaves)
-inflor.upregulated <- inflor.leaves[(inflor.leaves$log2FoldChange>0),]
+flower.vvi <- as.matrix(counts[row.names(counts) %in% flower,])
 
-## Filter differentially expressed genes between inflorescences and stems
-deseq1 <- results(dds, contrast=c("tissue", "inflorescence", "stem"))
-df.deseq1 <- as.data.frame(deseq1)
-res1.deseq1<-df.deseq1[(df.deseq1$log2FoldChange>1|df.deseq1$log2FoldChange< -1)& !is.na(df.deseq1$log2FoldChange),]
-inflor.stem <- res1.deseq1[(res1.deseq1$padj<0.05)&!is.na(res1.deseq1$padj),]
-inflor.stem$Gene <- row.names(inflor.stem)
+temp1 <- as.data.frame(merge(dict, dom, by.x = "V1", by.y = "Gene")) #Good for searching gene IDs
+flower <- gsub("$", ".t01", flower)
+flower <- as.data.frame(flower)
+flower.genes <- temp1[temp1$V2 %in% flower$flower,]
 
-## Filter differentially expressed genes between inflorescences and stems
-deseq1 <- results(dds, contrast=c("tissue", "Control", "stem"))
-df.deseq1 <- as.data.frame(deseq1)
-res1.deseq1<-df.deseq1[(df.deseq1$log2FoldChange>1|df.deseq1$log2FoldChange< -1)& !is.na(df.deseq1$log2FoldChange),]
-leaves.stem <- res1.deseq1[(res1.deseq1$padj<0.05)&!is.na(res1.deseq1$padj),]
-leaves.stem$Gene <- row.names(leaves.stem)
-stem.upregulated <- leaves.stem[(leaves.stem$log2FoldChange<0),]
+heatmap(flower.vvi, Rowv = NA, Colv = NA, margins=c(10,10))
 
-# Remove any upregulated stem genes
-test <- subset(inflor.upregulated, !(Gene %in% stem.upregulated$Gene))
+p <- plot_ly(x=colnames(flower.vvi), y=rownames(flower.vvi), z = flower.vvi, type = "heatmap")
+p
