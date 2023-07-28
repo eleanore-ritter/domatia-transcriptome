6d5c276d01b5bc2418b0f319f101ab7acc54d59f
eleanore-ritter <44507907+eleanore-ritter@users.noreply.github.com>
2023-07-28 17:41:51 -0400

    Update data_wrangling_for_GREAT.R

--- scripts/data_wrangling_for_GREAT.R
+++ scripts/data_wrangling_for_GREAT.R
@@ -1,6 +1,6 @@
 # Set working directory and load necessary packages
-setwd("C:/Users/rittere5/OneDrive - Michigan State University/Vitis-domatia/")
-#setwd("C:/Users/elean/OneDrive - Michigan State University/Vitis-domatia/")
+#setwd("C:/Users/rittere5/OneDrive - Michigan State University/Vitis-domatia/")
+setwd("C:/Users/elean/OneDrive - Michigan State University/Vitis-domatia/")
 
 ######################## DATA WRANGLING FOR GREAT ########################
 
@@ -42,28 +42,30 @@ genes.mat <- as.matrix(counts)
 heatmap(genes.mat, Rowv = NA, Colv = NA,margins=c(10,10))
 
 # List of genes that seem to be upregulated in inflorescences compared to leaf and stem tissue
-flower <- c("Vitvi02g00512",
-            "Vitvi04g00641",
-            "Vitvi03g01477",
+flower <- c(
+#Seems highly expressed in stem   "Vitvi02g00060",
+            "Vitvi01g01028",
+            "Vitvi04g01440",
+            "Vitvi06g00463",
+            "Vitvi04g02117",
+            "Vitvi08g01030",
+#Seems highly expressed in stem            "Vitvi13g00740",
+            "Vitvi15g00520",
+            "Vitvi02g00512",
             "Vitvi04g02123",
             "Vitvi07g01906",
-            "Vitvi07g00470",
-            "Vitvi09g00463",
             "Vitvi18g03417",
-            "Vitvi19g00369",
             "Vitvi19g00264",
-            "Vitvi11g00247",
+# Interesting gene, but seems to be moderately expressed in leaves            "Vitvi11g00247",
             "Vitvi11g00231",
             "Vitvi10g00854",
-            "Vitvi10g00663",
             "Vitvi10g00644",
             "Vitvi13g00829",
             "Vitvi12g02361",
             "Vitvi12g00725",
-            "Vitvi15g00776",
-            "Vitvi14g01928",
-            "Vitvi13g02416",
-            "Vitvi18g02654")
+            "Vitvi15g00776"
+# Highly expressed in Arabidopsis flowers, but somwhat expressed in Vvi stem and leaves            ,"Vitvi14g01928"
+            )
 
 flower.vvi <- as.matrix(counts[row.names(counts) %in% flower,])
 
@@ -76,3 +78,39 @@ heatmap(flower.vvi, Rowv = NA, Colv = NA, margins=c(10,10))
 
 p <- plot_ly(x=colnames(flower.vvi), y=rownames(flower.vvi), z = flower.vvi, type = "heatmap")
 p
+
+# Log transform
+flower.vvi.log2 <- log2(flower.vvi + 1)
+flower.vvi.Z <- t(scale(t(flower.vvi.log2)))
+col.order <- c(
+#Floral  
+  "Vitvi15g00776",
+  "Vitvi10g00854",
+  "Vitvi08g01030",
+  "Vitvi01g01028",
+  "Vitvi11g00231",
+  "Vitvi15g00520",
+# JMT, BSMT, or AAP2 (defense-related)
+  "Vitvi07g01906",
+  "Vitvi04g02123",
+  "Vitvi04g02117",
+  "Vitvi12g00725",
+#Trichome  
+  "Vitvi04g01440",
+#Metabolism  
+  "Vitvi02g00512",
+  "Vitvi19g00264",
+#Misc
+  "Vitvi13g00829",
+  "Vitvi06g00463",
+  "Vitvi10g00644",
+  "Vitvi12g02361"
+  
+)
+flower.vvi.Z <- flower.vvi.Z[col.order ,]
+heatmap(flower.vvi.Z, Rowv = NA, Colv = NA, margins=c(10,10))
+#plot_ly(x=colnames(flower.vvi.Z), y = rownames(flower.vvi.Z), z = flower.vvi.Z, type = "heatmap")      
+
+temp <- log2(genes.mat + 1)
+genes.mat1 <- temp[300:350,]
+plot_ly(x=colnames(genes.mat1), y = rownames(genes.mat1), z = genes.mat1, type = "heatmap") 
