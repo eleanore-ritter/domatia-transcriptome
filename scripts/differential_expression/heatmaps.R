68f11d6d940e1c2ffb360b09fc0f75e11b5fc8c8
eleanore-ritter <44507907+eleanore-ritter@users.noreply.github.com>
2023-09-07 16:02:12 -0400

    Update heatmaps.R

--- scripts/differential_expression/heatmaps.R
+++ scripts/differential_expression/heatmaps.R
@@ -1,7 +1,7 @@
 # Set working directory and load necessary packages
 
-#setwd("C:/Users/rittere5/OneDrive - Michigan State University/Vitis-domatia/")
-setwd("C:/Users/elean/OneDrive - Michigan State University/Vitis-domatia/")
+setwd("C:/Users/rittere5/OneDrive - Michigan State University/Vitis-domatia/")
+#setwd("C:/Users/elean/OneDrive - Michigan State University/Vitis-domatia/")
 
 library(DESeq2)
 library(dplyr)
@@ -126,7 +126,7 @@ auxin.genes <- c("AT5G57390", #AIL5 #Start of auxin synthesis regulators
                  "AT1G28130", #GH3.17
                  "AT1G73590", #PIN1 #Start of auxin transporters
                  "AT1G70940", #PIN3
-                 "AT1G70940", #WAT1
+                 "AT1G75500", #WAT1
                  "AT3G23050", #IAA7 #Start of transcriptional regulation via auxin signaling
                  "AT2G46990", #IAA20
                  "AT2G33310", #IAA13
@@ -158,13 +158,76 @@ colnames(genes.df2) <- c("588710 1 Control", "588710 2 Control", "588710 3 Contr
                          "588711 1 Control", "588711 2 Control", "588711 3 Control",
                          "588711 1 Domatia", "588711 2 Domatia", "588711 3 Domatia")
 
+rownames(genes.df2) <- c("GH3.6", "GH3.17-like", "Auxin efflux carrier component 1c",
+  "Auxin efflux carrier component 3-like", "WAT1", "IAA14", "IAA13-like", "Auxin-induced protein 22D-like (1)",
+  "Auxin-induced protein 22D-like (2)", "AUX22-like", "IAA9-like", "IAA27",
+  "LOC117913455", "SAUR21-like (1)", "SAUR21-like (2)", "SAUR21-like (3)", "X15-like",
+  "SAUR64-like", "SAUR67-like", "SAUR68-like")
+
 genes.mat <- as.matrix(genes.df2)
-logt <- log(genes.mat+0.01)
-temp1 <- t(apply(logt, 1, scale))
+
+# #Log transformed values below - I think it looks better not transformed
+# logt <- log(genes.mat+0.001)
+# temp1 <- t(apply(logt, 1, scale))
+# col_fun<-colorRamp2(seq(min(temp1),max(temp1),length.out=256),
+#                     colorRampPalette(c("blue","white","red"))(256))
+# Heatmap(temp1, cluster_rows = FALSE, cluster_columns = FALSE,col=col_fun)
+
+temp1 <- t(apply(genes.df2, 1, scale))
 col_fun<-colorRamp2(seq(min(temp1),max(temp1),length.out=256),
                     colorRampPalette(c("blue","white","red"))(256))
 Heatmap(temp1, cluster_rows = FALSE, cluster_columns = FALSE,col=col_fun)
 
+column_split = rep("Control 588710", 12)
+column_split[4:6] = "Domatia 588710"
+column_split[7:9] = "Control 588711"
+column_split[10:12] = "Domatia 588711"
+
+row_split = rep("Auxin synthases", 20)
+row_split[3:5] = "Auxin transporters"
+row_split[6:12] = "Transcriptional regulation\n via auxin signaling"
+row_split[13:20] = "Genes upregulated by auxin"
+
+#Legend to right
+Heatmap(temp1, 
+        cluster_rows = FALSE, 
+        cluster_columns = FALSE,
+        col=col_fun,
+        heatmap_legend_param = list(
+          title = "Z-Score", 
+          border = "black", 
+          legend_height = unit(6, "cm"),
+          title_gp = gpar(size = 14, fontface = "bold")),
+        column_split = factor(column_split, levels = c("Control 588710", "Domatia 588710", "Control 588711", "Domatia 588711")),
+        cluster_column_slices = FALSE,
+        column_gap = unit(2, "mm"),
+        border = TRUE,
+        row_split = factor(row_split, levels = c("Auxin synthases", "Auxin transporters", "Transcriptional regulation\n via auxin signaling", "Genes upregulated by auxin")),
+        row_title_rot = 0,
+        row_gap = unit(2, "mm")
+)
+#Legend on bottom
+htmp = Heatmap(temp1, 
+        cluster_rows = FALSE, 
+        cluster_columns = FALSE,
+        col=col_fun,
+        heatmap_legend_param = list(
+          title = "Z-Score", 
+          border = "black", 
+          legend_width = unit(6, "cm"),
+          title_gp = gpar(size = 14, fontface = "bold"),
+          direction = "horizontal"),
+        column_split = factor(column_split, levels = c("Control 588710", "Domatia 588710", "Control 588711", "Domatia 588711")),
+        cluster_column_slices = FALSE,
+        column_gap = unit(2, "mm"),
+        border = TRUE,
+        row_split = factor(row_split, levels = c("Auxin synthases", "Auxin transporters", "Transcriptional regulation\n via auxin signaling", "Genes upregulated by auxin")),
+        row_title_rot = 0,
+        row_gap = unit(2, "mm"),
+        row_names_gp = gpar(fontsize = 11)
+)
+
+draw(htmp, heatmap_legend_side="bottom")
 ######################## MAKE FIGURE 8 ########################
 dom.genes <- c("LOC117934313",
                "LOC117912178",
@@ -191,3 +254,25 @@ temp1 <- t(apply(logt, 1, scale))
 col_fun<-colorRamp2(seq(min(temp1),max(temp1),length.out=256),
                     colorRampPalette(c("blue","white","red"))(256))
 Heatmap(temp1, cluster_rows = FALSE, cluster_columns = FALSE,col=col_fun)
+
+####################### GETTING VRIPARIA PRODUCT NAMES ########################
+gff.id <- read.csv("vriparia-genomic-id.csv", sep=";", header = FALSE)
+
+test <- as.matrix(gff.id)
+genes<-matrix(grepl("^gene=",test),nrow(gff.id),ncol(gff.id))
+any(rowSums(genes)>1) #gene specified only once per row
+tmp<-which(genes,arr.ind=TRUE)
+genes<-gsub("^gene=","",test[genes])
+genes<-genes[match(1:nrow(test),tmp[,1])]
+products<-matrix(grepl("^product=",test),nrow(gff.id),ncol(gff.id))
+any(rowSums(products)>1) #products specified only once per row
+tmp<-which(products,arr.ind=TRUE)
+products<-gsub("^product=","",test[products])
+products<-products[match(1:nrow(test),tmp[,1])]
+dict <- as.data.frame(unique(cbind(genes,products)))
+
+temp1 <- na.omit(dict[dict$genes %in% dom.genes,])
+temp2 <- temp1[!duplicated(temp1$genes),]
+
+temp1 <- na.omit(dict[dict$genes %in% rownames(genes.df2),])
+temp2 <- temp1[!duplicated(temp1$genes),]
