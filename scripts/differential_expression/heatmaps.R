a8aaea7259740253ec523cb17af52cc659169b54
eleanore-ritter <44507907+eleanore-ritter@users.noreply.github.com>
2023-09-13 15:58:32 -0400

    Update heatmaps.R

--- scripts/differential_expression/heatmaps.R
+++ scripts/differential_expression/heatmaps.R
@@ -1,7 +1,7 @@
 # Set working directory and load necessary packages
 
-#setwd("C:/Users/rittere5/OneDrive - Michigan State University/Vitis-domatia/")
-setwd("C:/Users/elean/OneDrive - Michigan State University/Vitis-domatia/")
+setwd("C:/Users/rittere5/OneDrive - Michigan State University/Vitis-domatia/")
+#setwd("C:/Users/elean/OneDrive - Michigan State University/Vitis-domatia/")
 
 library(DESeq2)
 library(dplyr)
@@ -43,7 +43,8 @@ final<-cbind(degenes,all[match(degenes$Gene,all$gene),])
 # Copy Arabidopsis orthologs
 writeClipboard(paste0(as.vector(na.omit(gsub("\\.[0-9]", "", final$Arabidopsis_blast_hit)))))
 
-
+overlap <- domcon.710[domcon.710$Gene %in% domcon.711$Gene,]
+writeClipboard(paste0(as.vector(na.omit(gsub("\\.[0-9]", "", overlap$ensembl_gene_id)))))
 ######################## COMPARING COMPLEX HEATMAP TO OTHER OPTIONS ########################
 # Make test matrix
 genes.df <- domdom[c(23,24,25,26,27,28,29,30,31,32,33,34)]
@@ -97,25 +98,149 @@ Heatmap(temp2, cluster_rows = FALSE, cluster_columns = FALSE,col=col_fun)
 ######################## MAKE FIGURE 3 ########################
 cellwall <- read.csv("GO-term-enrichment/DE_genes_Control_V_Domatia_588710_cell_wall_gene_list.csv",
                      sep="\t", row.names=NULL, header = FALSE)
-genes.cellwall <- cellwall$V2
-genes.df <- domcon.710[domcon.710$ensembl_gene_id %in% genes.cellwall,]
+
+cellwall.genes <- c("AT5G13870", #XTH5 #Start of xyloglucan synthesis and modification
+                    "AT2G14620", #XTH10
+                    "AT4G14130", #XTH15
+                    "AT4G25810", #XTH23
+                    "AT2G36870", #XTH32
+                    "AT5G20950", #BGLC1
+                    "AT1G19300", #GATL1 #Start of xylan synthesis and modification
+                    "AT3G50220", #IRX15 
+                    "AT3G18660", #GUX1
+                    "AT5G54690", #GAUT12
+                    "AT1G09610", #GXM1
+                    "AT1G33800", #GXM3
+                    "AT1G73140", #TBL31
+                    "AT2G38320", #TBL34
+                    "AT3G05620", #PME22 #Start of pectin biosynthesis and modification
+                    "AT3G43270", #PME32
+                    "AT1G23200", #PME6
+                    "AT2G26440", #PME12
+                    "AT1G11580", #PME18
+                    "AT5G19730", #PME53
+                    "AT1G05310", #PME8
+                    "AT5G63180", #PLL15
+                    "AT1G67750", #PLL16
+                    "AT3G07010", #PLL20
+                    "AT3G29360", #UGD2
+                    "AT5G03260", #LAC11 #Start of lignin biosynthesis and modification
+                    "AT5G60020", #LAC17
+                    "AT2G38080", #LAC4
+                    "AT4G36220", #FAH1
+                    "AT4G34050", #CCOAOMT1
+                    "AT1G67980", #CCOAMT
+                    "AT5G05340", #PER52
+                    "AT4G01070" #UGT72B1
+                    )
+
+genes.df <- domcon.710[domcon.710$ensembl_gene_id %in% cellwall.genes,]
 genes.df1 <- genes.df[genes.df$Gene %in% domcon.711$Gene,] # Run if we only want genes shared between genotypes
+genes.df1 <- genes.df1[order(match(genes.df1$ensembl_gene_id,cellwall.genes)),]
 genes.df2 <- genes.df1[c(23,24,25,26,27,28,29,30,31,32,33,34)] # Modify if code above is run
 colnames(genes.df2) <- c("588710 1 Control", "588710 2 Control", "588710 3 Control", 
-                        "588710 1 Domatia", "588710 2 Domatia", "588710 3 Domatia" ,
-                        "588711 1 Control", "588711 2 Control", "588711 3 Control",
-                        "588711 1 Domatia", "588711 2 Domatia", "588711 3 Domatia")
+                         "588710 1 Domatia", "588710 2 Domatia", "588710 3 Domatia" ,
+                         "588711 1 Control", "588711 2 Control", "588711 3 Control",
+                         "588711 1 Domatia", "588711 2 Domatia", "588711 3 Domatia")
+
+rownames(genes.df2) <- c("Probable xyloglucan endotransglucosylase/\nhydrolase protein B",
+                         "Probable xyloglucan endotransglucosylase/\nhydrolase protein 10",
+                         "Xyloglucan endotransglucosylase/\nhydrolase 2-like",
+                         "Pectinesterase",
+                         "Glucuronoxylan\n4-O-methyltransferase 3",
+                         "Probable pectinesterase 53",
+                         "Pectate lyase-like",
+                         "Probable pectate lyase 5",
+                         "Laccase-11-like",
+                         "Laccase-17-like (1)",
+                         "Laccase-17-like (2)",
+                         "Laccase-17-like (3)",
+                         "Laccase-17-like (4)",
+                         "Laccase-17-like (5)",
+                         "Hydroquinone\nglucosyltransferase-like")
+
 genes.mat <- as.matrix(genes.df2)
-logt <- log(genes.mat+0.01)
-temp1 <- t(apply(logt, 1, scale))
+
+# #Log transformed values below - I think it looks better not transformed
+# logt <- log(genes.mat+0.001)
+# temp1 <- t(apply(logt, 1, scale))
+# col_fun<-colorRamp2(seq(min(temp1),max(temp1),length.out=256),
+#                     colorRampPalette(c("blue","white","red"))(256))
+# Heatmap(temp1, cluster_rows = FALSE, cluster_columns = FALSE,col=col_fun)
+
+temp1 <- t(apply(genes.df2, 1, scale))
 col_fun<-colorRamp2(seq(min(temp1),max(temp1),length.out=256),
                     colorRampPalette(c("blue","white","red"))(256))
 Heatmap(temp1, cluster_rows = FALSE, cluster_columns = FALSE,col=col_fun)
 
+column_split = rep("Control\n 588710", 12)
+column_split[4:6] = "Domatia\n 588710"
+column_split[7:9] = "Control\n 588711"
+column_split[10:12] = "Domatia\n 588711"
+
+row_split = rep("Xyloglucan\nbiosynthesis\nand modification", 15)
+row_split[4] = "Xylan biosynthesis\n and modification"
+row_split[6:8] = "Pectin biosynthesis\n and modification"
+row_split[9:15] = "Lignin biosynthesis\n and modification"
+
+#Legend to right
+# Heatmap(temp1, 
+#         cluster_rows = FALSE, 
+#         cluster_columns = FALSE,
+#         col=col_fun,
+#         heatmap_legend_param = list(
+#           title = "Z-Score", 
+#           border = "black", 
+#           legend_height = unit(6, "cm"),
+#           title_gp = gpar(size = 14, fontface = "bold")),
+#         column_split = factor(column_split, levels = c("Control 588710", "Domatia 588710", "Control 588711", "Domatia 588711")),
+#         cluster_column_slices = FALSE,
+#         column_gap = unit(2, "mm"),
+#         border = TRUE,
+#         row_split = factor(row_split, levels = c("Auxin synthases", "Auxin transporters", "Transcriptional regulation\n via auxin signaling", "Genes upregulated\n by auxin")),
+#         row_title_rot = 0,
+#         row_gap = unit(2, "mm")
+# )
+
+#Legend on bottom
+htmp = Heatmap(temp1, 
+               cluster_rows = FALSE, 
+               cluster_columns = FALSE,
+               col=col_fun,
+               heatmap_legend_param = list(
+                 title = "Z-Score", 
+                 border = "black", 
+                 legend_width = unit(6, "cm"),
+                 title_gp = gpar(size = 12, fontface = "bold"),
+                 direction = "horizontal"),
+               column_split = factor(column_split, levels = c("Control\n 588710", "Domatia\n 588710", "Control\n 588711", "Domatia\n 588711")),
+               cluster_column_slices = FALSE,
+               column_gap = unit(2, "mm"),
+               border = TRUE,
+               row_split = factor(row_split, levels = c("Xyloglucan\nbiosynthesis\nand modification", "Xylan biosynthesis\n and modification", 
+                                                        "Pectin biosynthesis\n and modification", "Lignin biosynthesis\n and modification")),
+               row_title_rot = 0,
+               row_gap = unit(2, "mm"),
+               row_names_max_width = max_text_width(
+                 rownames(temp1), 
+                 gp = gpar(fontsize = 10)
+               ),
+               row_names_gp = gpar(fontsize = 10),
+               row_title_gp = gpar(fontsize = 12, fontface = "bold")
+)
+
+
+draw(htmp, heatmap_legend_side="bottom")
+
 ######################## MAKE FIGURE 4 ########################
 
-genes.meta <- c("",
-                )
+meta.genes <- c("AT5G09220", #AAP2 #Start of amino acid transport
+                "AT1G77380", #AAP3
+                "AT1G10010", #AAP8
+                "AT5G04770", #CAT6
+                "AT2G39510", #UMAMIT14
+                #cARB TRANSPORT AND META
+)
 
 ######################## MAKE FIGURE 5 ########################
 auxin <- read.csv("GO-term-enrichment/for-heatmaps/auxin/588710-auxin.txt",
@@ -272,8 +397,8 @@ products<-gsub("^product=","",test[products])
 products<-products[match(1:nrow(test),tmp[,1])]
 dict <- as.data.frame(unique(cbind(genes,products)))
 
-temp1 <- na.omit(dict[dict$genes %in% dom.genes,])
-temp2 <- temp1[!duplicated(temp1$genes),]
+# temp1 <- na.omit(dict[dict$genes %in% dom.genes,])
+# temp2 <- temp1[!duplicated(temp1$genes),]
 
 temp1 <- na.omit(dict[dict$genes %in% rownames(genes.df2),])
 temp2 <- temp1[!duplicated(temp1$genes),]
