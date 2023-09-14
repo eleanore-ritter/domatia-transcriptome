01d47b3c5ebc35cb2d275bd1e1838750cbc2ed4e
eleanore-ritter <44507907+eleanore-ritter@users.noreply.github.com>
2023-09-14 14:33:36 -0400

    Update heatmaps.R

--- scripts/differential_expression/heatmaps.R
+++ scripts/differential_expression/heatmaps.R
@@ -239,8 +239,106 @@ meta.genes <- c("AT5G09220", #AAP2 #Start of amino acid transport
                 "AT1G10010", #AAP8
                 "AT5G04770", #CAT6
                 "AT2G39510", #UMAMIT14
-                #cARB TRANSPORT AND META
+                "AT3G28960", #hexon #Start of carb metabolism
+                "AT1G73370", #SUS6
+                "AT1G12240", #VIN2
+                "AT1G47840", #HXK3
+                "AT4G10260", #FRK4
+                "AT5G49690", #UGT91C1
+                "AT1G22360", #AtUGT85A2
+                "AT1G22400", #UGT85A1
+                "AT4G01070", #GT72B1
+                "AT2G43820", #GT
+                "AT5G40390", #RFS5 
+                "AT4G15920", #SWEET17 #Start of carb transport
+                "AT1G22710", #SUC2
+                "AT1G35720" #ANN1
 )
+
+genes.df <- domcon.710[domcon.710$ensembl_gene_id %in% meta.genes,]
+genes.df1 <- genes.df[genes.df$Gene %in% domcon.711$Gene,] # Run if we only want genes shared between genotypes
+genes.df1 <- genes.df1[order(match(genes.df1$ensembl_gene_id,meta.genes)),]
+genes.df2 <- genes.df1[c(23,24,25,26,27,28,29,30,31,32,33,34)] # Modify if code above is run
+colnames(genes.df2) <- c("588710 1 Control", "588710 2 Control", "588710 3 Control", 
+                         "588710 1 Domatia", "588710 2 Domatia", "588710 3 Domatia" ,
+                         "588711 1 Control", "588711 2 Control", "588711 3 Control",
+                         "588711 1 Domatia", "588711 2 Domatia", "588711 3 Domatia")
+
+rownames(genes.df2) <- c("Amino acid permease 3-like (1)",
+                         "Amino acid permease 3-like (2)",
+                         "Amino acid permease 8-like",
+                         "Cationic amino acid transporter 6",
+                         "WAT1-related protein",
+                         "Amino acid transporter AVT1I-like (1)",
+                         "Amino acid transporter AVT1I-like (2)",
+                         "Sucrose synthase 7-like",
+                         "Acid beta-fructofuranosidase",
+                         "Hexokinase-2",
+                         "Probable fructokinase-5",
+                         "UDP-glycosyltransferase 91C1-like",
+                         "7-deoxyloganetin glucosyltransferase-like",
+                         "7-deoxyloganetic acid glucosyltransferase-like",
+                         "Hydroquinone glucosyltransferase-like",
+                         "UDP-glycosyltransferase 74F2-like",
+                         "Galactinol--sucrose galactosyltransferase-like (1)",
+                         "Galactinol--sucrose galactosyltransferase-like (2)",
+                         "Bidirectional sugar transporter SWEET17",
+                         "Sucrose transport protein SUC2-like",
+                         "Annexin D1",
+                         "Annexin D4-like")
+
+genes.mat <- as.matrix(genes.df2)
+
+# #Log transformed values below - I think it looks better not transformed
+# logt <- log(genes.mat+0.001)
+# temp1 <- t(apply(logt, 1, scale))
+# col_fun<-colorRamp2(seq(min(temp1),max(temp1),length.out=256),
+#                     colorRampPalette(c("blue","white","red"))(256))
+# Heatmap(temp1, cluster_rows = FALSE, cluster_columns = FALSE,col=col_fun)
+
+temp1 <- t(apply(genes.df2, 1, scale))
+col_fun<-colorRamp2(seq(min(temp1),max(temp1),length.out=256),
+                    colorRampPalette(c("blue","white","red"))(256))
+Heatmap(temp1, cluster_rows = FALSE, cluster_columns = FALSE,col=col_fun)
+
+column_split = rep("Control\n 588710", 12)
+column_split[4:6] = "Domatia\n 588710"
+column_split[7:9] = "Control\n 588711"
+column_split[10:12] = "Domatia\n 588711"
+
+row_split = rep("Amino acid\ntransport", 22)
+row_split[8:18] = "Carbohydrate\nmetabolism"
+row_split[19:22] = "Carbohydrate\ntransport"
+
+#Legend on bottom
+htmp = Heatmap(temp1, 
+               cluster_rows = FALSE, 
+               cluster_columns = FALSE,
+               col=col_fun,
+               heatmap_legend_param = list(
+                 title = "Z-Score", 
+                 border = "black", 
+                 legend_width = unit(6, "cm"),
+                 title_gp = gpar(size = 12, fontface = "bold"),
+                 direction = "horizontal"),
+               column_split = factor(column_split, levels = c("Control\n 588710", "Domatia\n 588710", "Control\n 588711", "Domatia\n 588711")),
+               cluster_column_slices = FALSE,
+               column_gap = unit(2, "mm"),
+               border = TRUE,
+               row_split = factor(row_split, levels = c("Amino acid\ntransport", "Carbohydrate\nmetabolism", 
+                                                        "Carbohydrate\ntransport")),
+               row_title_rot = 0,
+               row_gap = unit(2, "mm"),
+               row_names_max_width = max_text_width(
+                 rownames(temp1), 
+                 gp = gpar(fontsize = 10)
+               ),
+               row_names_gp = gpar(fontsize = 10),
+               row_title_gp = gpar(fontsize = 12, fontface = "bold")
+)
+
+
+draw(htmp, heatmap_legend_side="bottom")
 
 ######################## MAKE FIGURE 5 ########################
 auxin <- read.csv("GO-term-enrichment/for-heatmaps/auxin/588710-auxin.txt",
@@ -354,6 +452,17 @@ htmp = Heatmap(temp1,
 )
 
 draw(htmp, heatmap_legend_side="bottom")
+######################## MAKE FIGURE 6 ########################
+
+biotic.genes <- c("AT1G12220", #NBS-LRR class
+                  "AT3G51570",
+                  "AT5G17680",
+                  "AT5G36930",
+                  "AT3G61460", #BRH1 #Chitin
+                  "AT5G63380", #JA Biosynthesis #JA signaling
+                  
+)
+
 ######################## MAKE FIGURE 8 ########################
 dom.genes <- c("LOC117934313",
                "LOC117912178",
